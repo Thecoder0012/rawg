ef0346f053ec785471f6fe9ec9826e24c6f8afba
Mardin <mardineliassi57@gmail.com>
2024-02-12 09:59:03 +0100

    responsive design

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+      >
+        <GridItem bg="orange.300" gridArea={"nav"}>
+          Nav
+        </GridItem>
+        <Show above="lg">
+          <GridItem bg="pink.300" gridArea={"aside"}>
+            Aside
+          </GridItem>
+        </Show>
+
+        <GridItem bg="green.300" gridArea={"main"}>
+          Main
+        </GridItem>
+      </Grid>
+    </>
+  );
+}
+
+export default App;
